704f310f62ebabb0dd358fea2cedee6dfdce52e5
Michael Pollard <mpollard@goclg.com>
2022-12-01 15:04:58 -0700

    minor syntax updates to webpack dev setup

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.css') }}">
+    <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ 'css/app.css' }}">
     <title>AngularJS & Laravel Gallery App</title>
 </head>
 <body>
